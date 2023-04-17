6445a70384fff86ffb8867d9ab9db815e59b5653
lidmuspaper <90637834+lidmuspaper@users.noreply.github.com>
2023-04-17 14:38:54 -0400

    Hardcode password + test route

--- flask-app/src/persona1/persona1.py
+++ flask-app/src/persona1/persona1.py
@@ -5,6 +5,16 @@ from src import db
 persona1 = Blueprint('persona1', __name__)
 
 # Get all plants from the DB
-@persona1.route('/persona1', methods=['GET'])
+@persona1.route('/get_all_plants', methods=['GET'])
 def get_plants():
-	pass
+    cursor = db.get_db().cursor()
+    cursor.execute('select plant_id from Plant')
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
