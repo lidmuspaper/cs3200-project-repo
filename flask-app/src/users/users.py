6bc760430a7c06b4e3b484e9a2e62e0c6491c3ee
lidmuspaper <90637834+lidmuspaper@users.noreply.github.com>
2023-04-17 18:42:51 -0400

    Update users.py
    add route for new user

--- flask-app/src/users/users.py
+++ flask-app/src/users/users.py
@@ -18,3 +18,32 @@ def get_types():
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+
+# Add a new user
+@users.route('/add_new_user', methods=['POST'])
+def add_user():
+    data = request.json
+    current_app.logger.info(data)
+
+    full_name = data['full_name']
+    email = data['email']
+    experience_level = data['experience_level']
+    city = data['city']
+    state = data['state']
+    zip_code = data['zip_code']
+
+    query = 'INSERT INTO User (full_name, email, Experience_level, city, state, zip_code) VALUES ('
+    query += '"' + full_name + '","'
+    query += email + '",'
+    query += str(experience_level) + ', "'
+    query += city + '","'
+    query += state + '",'
+    query += str(zip_code) + ')'
+
+    current_app.logger.info(query)
+
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return query
