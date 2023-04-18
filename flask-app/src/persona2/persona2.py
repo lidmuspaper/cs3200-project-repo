d3b75783a191033c411a2f864b1628b70d95f117
alyssa <demarco.a@northeastern.edu>
2023-04-18 18:09:53 -0400

    t

--- flask-app/src/persona2/persona2.py
+++ flask-app/src/persona2/persona2.py
@@ -7,3 +7,21 @@ persona2 = Blueprint('persona2', __name__)
 @persona2.route('/persona1', methods=['GET'])
 def get_plants():
 	pass
+
+# Get this user's weather from the DB
+@persona2.route('/get_weather', methods=['GET'])
+def get_weather():
+    data = request.json
+    current_app.logger.info(data)
+
+    cursor = db.get_db().cursor()
+    cursor.execute('select * from Weather WHERE user_id = ' + str(data['user_id']))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
